6d542755f8ef8d3efd605a580247a0da95e69174
Marcos <marcoswodbarros@outlook.com>
2023-08-24 14:35:17 -0300

    Mobile: Updating Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,16 @@
-function showMenu() {
+const  showMenu = function() {
+    let main = document.querySelector('main');
     let openMenu = document.querySelector('.nav__ul');
     let menuIcons = document.querySelector('.nav__menu--icons');
 
     if (openMenu.classList.contains('nav__ul--expanded')) {
         openMenu.classList.remove('nav__ul--expanded');
         menuIcons.src = 'assets/images/icon-menu.svg';
+        main.style.position = 'static';
 
     } else {
         openMenu.classList.add('nav__ul--expanded');
         menuIcons.src = 'assets/images/icon-menu-close.svg';
+        main.style.position = 'fixed';
     }
-
 }
